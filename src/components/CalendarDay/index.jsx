7954de34c03d2f16bb4bf39dd84539f2e59d5bcd
gustavomarmo <gsmarmo@gmail.com>
2026-02-10 01:45:53 -0300

    feature: componente de box de dia

---
+++ src/components/CalendarDay/index.jsx
@@ -0,0 +1,25 @@
+import styles from './CalendarDay.module.css';
+
+export function CalendarDay({ day, type = 'current', isToday = false, events = [], onClick }) {
+    
+    return (
+        <div 
+            className={`
+                ${styles.container} 
+                ${type !== 'current' ? styles.otherMonth : ''}
+                ${isToday ? styles.today : ''}
+            `}
+            onClick={onClick}
+        >
+            <span className={styles.dayNumber}>{day}</span>
+            
+            <div className={styles.eventList}>
+                {events.map((evt, idx) => (
+                    <div key={idx} className={styles.eventItem} title={evt}>
+                        {evt}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
